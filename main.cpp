58fe227ba3603842827e1b8e57b8638e3616b034
Alex Studer <alex@studer.dev>
2023-10-13 17:24:13 -0400

    pass argc and argv to gtk, allowing it to handle command-line flags

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ int main(int argc, char * argv[])
 
 	GtkApplication * app = gtk_application_new("space.myhomework.desktop", G_APPLICATION_FLAGS_NONE);
 	g_signal_connect(app, "activate", G_CALLBACK(activate), nullptr);
-	g_application_run(G_APPLICATION(app), 0, NULL);
+	g_application_run(G_APPLICATION(app), argc, argv);
 	g_object_unref(app);
 
 	return 0;
